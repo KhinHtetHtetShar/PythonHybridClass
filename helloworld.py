00a4b86f0b6e28dbe9a9281850c49baffa1210e2
KhinHtetHtetShar <khinhtethtetshar1994@gmail.com>
2019-10-27 18:17:43 +0630

    Tax Calculator

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+charge = 10000
+tax = 50/1000
+total tax = charge * tax
+total paid = charge + total tax
+total paid = ?
+
+#Problem
+
+charge = 15000
+tax = 50 / 1000
+total tax = charge * tax
+totaltax
+750.0
+total_paid = charge + totaltax
+total_paid
+15750.0
